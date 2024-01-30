bab5e2eb83ebb0edf6db3d1bfdf6720a23e0d5f0
Pla9er <drugiekonto_99@o2.pl>
2024-01-30 23:42:47 +0100

    removed unnecessary argument

--- src/app/api/results/route.ts
+++ src/app/api/results/route.ts
@@ -1,8 +1,7 @@
 import VotingPosition from "@/models/voting_position";
-import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(request: NextApiRequest) {
+export async function GET() {
   let res = await VotingPosition.aggregate([
     {
       $set: {
