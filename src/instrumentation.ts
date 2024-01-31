ed25fc0f45448ce339e70f748cd100655b396fb1
Pla9er9 <drugiekonto_99@o2.pl>
2024-01-31 12:32:56 +0100

    loading voting data

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,19 @@
 import { connectMongoDB } from "./lib/mongodb";
+import VotingPosition from "./models/voting_position";
 
 export async function register() {
   await connectMongoDB();
+  if (!await hasData()) {
+      await addData()
+  }
+}
+
+async function hasData(): Promise<boolean> {
+  return await VotingPosition.exists({}) !== null
+}
+
+async function addData(): Promise<void>  {
+  const fs = require('fs')
+  const file = await fs.readFile("./data.json")
+  const data = JSON.parse(file)
 }
